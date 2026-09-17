bb9555ec752bc8bb6c05de02790441867dad6641
mohamedamiri12 <amirimohamed1999@gmail.com>
2026-09-17 12:05:58 +0100

    angular material customization

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1 class="bg-red-500">Hello, {{ title() }}</h1>
+    <h1 class="text-2xl p-6">Hello, {{ title() }}</h1>
 
     <router-outlet />
   `,
